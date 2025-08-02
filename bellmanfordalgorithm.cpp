816c074f9db013e49c8e82c97e3e6178a958ebe0
mahak540 <162689029+mahak540@users.noreply.github.com>
2025-08-02 18:13:37 +0530

    Create bellmanfordalgorithm.cpp

---
+++ bellmanfordalgorithm.cpp
@@ -0,0 +1,23 @@
+vector<int> bellmanFord(int V, vector<vector<int>>& edges, int src) {
+        // Code here
+        vector<int>dist(V,1e8);
+        dist[src]=0;
+        for(int i=0;i<V;i++)
+        {
+            for(vector<int>edge:edges)
+            {
+                int u=edge[0];
+                int v=edge[1];
+                int w=edge[2];
+                
+                if(dist[u]!=1e8 && dist[u]+w < dist+[v])
+                {
+                    if(i==V-1)
+                    return {-1};
+                    
+                    dist[v]=dist[u]+w;
+                }
+            }
+        }
+        return dist;
+    }
